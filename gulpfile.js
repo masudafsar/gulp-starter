08f1a9bfa2d762d48723bc50a9a4596f945ca5a6
Masud Afsar <masud.afsar@gmail.com>
2017-05-26 21:18:02 +0430

    Implement sourcemap and clean tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gutil = require('gulp-util');
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
+var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 var ejs = require('gulp-ejs');
 
@@ -19,14 +20,24 @@ gulp.task('server', ['watch'], function () {
     });
 });
 
+gulp.task('build', [
+    'build:css',
+    'build:js',
+    'build:html'
+]);
+
 gulp.task('build:css', [], function () {
     gulp.src('./src/scss/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css'))
         .pipe(connect.reload());
 });
 
-gulp.task('build:ejs', [], function () {
+gulp.task('build:js', [], function () {});
+
+gulp.task('build:html', [], function () {
     gulp.src('./pages/*.ejs')
         .pipe(ejs(null, null, {
             ext: '.html'
@@ -35,9 +46,34 @@ gulp.task('build:ejs', [], function () {
 });
 
 gulp.task('watch', [
-    'watch:css'
+    'watch:css',
+    'watch:js',
+    'watch:html'
 ]);
 
 gulp.task('watch:css', [], function () {
     gulp.watch('./src/scss/**/*.scss', ['build:css']);
+});
+
+gulp.task('watch:js', [], function () {
+    gulp.watch('./src/js/**/*.js', ['build:js']);
+});
+
+gulp.task('watch:html', [], function () {
+    gulp.watch('./src/scss/**/*.ejs', ['build:html']);
+});
+
+gulp.task('clean', [
+    'clean:demo',
+    'clean:dist'
+]);
+
+gulp.task('clean:demo', [], function () {
+    gulp.src('./demo/*', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('clean:dist', [], function () {
+    gulp.src('./dist/', {read: false})
+        .pipe(clean());
 });
