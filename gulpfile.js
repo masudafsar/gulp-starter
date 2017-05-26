b7106fce67087331383d31e900eb64bb6ff55ba3
Masud Afsar <masud.afsar@gmail.com>
2017-05-26 16:15:42 +0430

    Implement scss compiler and live reload webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var connect = require('gulp-connect');
+var sass = require('gulp-sass');
 
 gulp.task('default', [], function () {
     // write default tasks here
+});
+
+gulp.task('server', ['watch'], function () {
+    connect.server({
+        root: './',
+        livereload: true
+    });
+});
+
+gulp.task('build:css', [], function () {
+    gulp.src('./src/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch', [
+    'watch:css'
+]);
+
+gulp.task('watch:css', [], function () {
+    gulp.watch('./src/scss/**/*.scss', ['build:css']);
 });
