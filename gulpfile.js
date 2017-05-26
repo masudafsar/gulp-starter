96a3a2ef60230fc0cbfbe75334e44ad54a769300
Masud Afsar <masud.afsar@gmail.com>
2017-05-26 16:19:58 +0430

    Implement ejs compiler

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,11 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
+var ejs = require('gulp-ejs');
 
 gulp.task('default', [], function () {
     // write default tasks here
@@ -21,6 +24,14 @@ gulp.task('build:css', [], function () {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
         .pipe(connect.reload());
+});
+
+gulp.task('build:ejs', [], function () {
+    gulp.src('./pages/*.ejs')
+        .pipe(ejs(null, null, {
+            ext: '.html'
+        }).on('error', gutil.log))
+        .pipe(gulp.dest('./demo'));
 });
 
 gulp.task('watch', [
